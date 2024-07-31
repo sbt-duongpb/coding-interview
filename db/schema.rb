25182e6ab3b8c232d7bbafe80e3b554640e56b9e
duongpb <duongu3t@gmail.com>
2024-07-31 16:00:27 +0700

    crud user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_31_013039) do
+ActiveRecord::Schema.define(version: 2024_07_31_085158) do
 
   create_table "articles", force: :cascade do |t|
     t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
